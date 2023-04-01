096079f34415e66c8cabd034098ed443e415ded3
Chiniune <vothitrinh2002@gmail.com>
2023-04-01 13:14:29 +0700

    all oke

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,10 +1,17 @@
-module com.example.alsbrowser {
+module com.example.browseral {
     requires javafx.controls;
     requires javafx.fxml;
-    requires java.sql;
     requires javafx.web;
+    requires java.sql;
+    requires ipinfo.api;
+    requires json.simple;
+    requires com.google.gson;
+    requires jjwt;
+//    requires news.api.java;
 
 
-    opens com.example.alsbrowser to javafx.fxml;
-    exports com.example.alsbrowser;
+    opens com.example.browseral to javafx.fxml;
+    exports com.example.browseral;
+    exports com.example.browseral.controllers;
+    opens com.example.browseral.controllers to javafx.fxml;
 }
